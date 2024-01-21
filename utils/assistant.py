f22077c7a522015abe870fa45c241b13e5dbc888
Kyle Condron <condronkyle@gmail.com>
2024-01-21 15:15:56 +0800

    setup

---
+++ utils/assistant.py
@@ -0,0 +1,157 @@
+from thread import Thread
+from utils import send_message_and_get_response
+
+class Assistant:
+    def __init__(self, client, name, instructions, model, assistant_id=None, thread_id=None):
+        self.client = client
+        self.name = name
+        self.instructions = instructions
+        self.model = model
+        self.thread = Thread(client, thread_id)  # Associate a Thread instance with each Assistant
+
+        if assistant_id:
+            self.fetch_assistant_details()
+
+        self.assistant_id = assistant_id or self.create_assistant()
+
+    def create_assistant(self):
+        assistant = self.client.client.beta.assistants.create(
+            name=self.name,
+            instructions=self.instructions,
+            model=self.model
+        )
+        return assistant.id
+
+    def fetch_assistant_details(self):
+        # Fetch and set the assistant details based on the existing assistant_id
+        # Note: You might need to use an appropriate method from the client API
+        # to retrieve the details of an assistant. This is a placeholder.
+        assistant_details = self.client.client.beta.assistants.retrieve(
+            assistant_id=self.assistant_id
+        )
+        # Assuming the API returns details that can be used to set up the assistant
+        self.instructions = assistant_details.instructions
+        self.model = assistant_details.model
+
+    def send_message_and_get_response(self, message_content):
+        return send_message_and_get_response(self.client.client, self.thread.thread_id, self.assistant_id, message_content)
+
+    def is_final_agent(self):
+        return self.name == "K"  # Assuming 'K' is the final agent in the workflow
+
+    # Function to determine the targeted agent from a message
+    def determine_target_agent(self, message, agents_dict):
+        #TODO switch statement for the diff current agents
+        content = message.content
+        if '[To E]' in content:
+            return agents_dict["E"], agents_dict["E"].thread.thread_id, message.extract_delivery_message() + "Remember, if you're ready to code, don't tell me that. Just start coding."
+        # bug for when E tries to multitask
+        elif ('[To P]' in content) and ('[To T]' in content):
+            return agents_dict["E"], agents_dict["E"].thread.thread_id, "You can only message one person at a time. Send your entire code to T for testing."
+        elif '[To P]' in content:
+            return agents_dict["P"], agents_dict["P"].thread.thread_id, message.extract_delivery_message()
+        elif '[To T]' in content:
+            # Send T the whole message
+            return agents_dict["T"], agents_dict["T"].thread.thread_id, message.content
+        elif '[To K]' in content:
+            return 'K', 'K', message.extract_delivery_message()
+        else:
+            return self, self.thread.thread_id, "This is the manager stepping in. Go ahead and do the work in your next message, and send it to your relevant teammate. And remember, you must always end your response with a message to one of your teammates by using [To <X>]."
+            # TODO: Logic to re-prompt for more info
+            # TODO: switch on which agent
+
+    
+class ProductManager(Assistant):
+    def __init__(self, client, assistant_id=None, thread_id=None):
+        super().__init__(
+            client,
+            "P",
+            """You are an exceptional Product Manager eager to build AND deliver new technology.
+            I trust you to work independently with our engineers and drive them to success.
+            You have one engineer on our team, named E. You and E have a great relationship. Communicate with E by typing "[To E] <message>".
+            Be concise and trust E to work independently.
+            After iteration with E, when you think the deliverables are in a good state, you can then reach out to me (I am K) and inform me by typing "[To K] <message>".
+            I will likely have followup for you.
+            It is a requirement of our systems that you keep the conversation going, so you must always ultimately send information to E in your response.
+            Additionally, don't ever tell anyone you will wait for them to get back to you. Always ensure you are specifically asking them to give a response to you.
+            If E is giving general status updates, do not encourage this. Tell him to stop messaging you, and instead write code and send it to T.""",
+            "gpt-4-1106-preview",
+            assistant_id=assistant_id,
+            thread_id=thread_id
+        )
+
+class Engineer(Assistant):
+    def __init__(self, client, assistant_id=None, thread_id=None):
+        super().__init__(
+            client,
+            "E",
+            """You are a phenomenal senior engineer, capable of designing, architecting, and coding robust and optimized solutions.
+            You have a great relationship with your Product Manager, P, who will give you requirements.
+            You can always ask for clarification from P, but remember, we all expect you to write and deliver code.
+            Don't wait to be told precise instructions, be confident and write code.
+            You also have a great relationship with a Test Engineer, named T.
+            Every message of yours should either be getting clarity from P, or writing code and sending it to T.
+            Remember, when you write to T, you should be writing code! Don't tell T you are going to start writing code soon, just write it!
+            Do not write a message saying you will do it soon, that is a waste of time. Just go ahead and do it.
+            Do not message P just to tell him you are going to get started.
+            When you write code, you should always send it to T and ask T to test it.
+            You can do so by typing "[To T] <message>".
+            Any text you write before "[To T]" as you work is fine, but only the message after will get delivered to T, and should include your code to test.
+            Again, make sure you paste your code AFTER you write "[To T]", that's the only way he can see it.
+            You cannot send T files, so you'll have to write out the entire code in your message to T.
+            This message should include your code as well as a request for testing, as well as testing instructions if necessary.
+            If T has issues, he will tell you and we expect you to work with him to resolve those issues iteratively.
+            Once T has signed off on your changes, you can then message back to P by typing "[To P] <message>".
+            Any text you write before "[To P]" as you work is fine, but only the message after will get delivered to P.
+            P may also have iterative followup with you. If P asks you to change code, remember to test again with T.
+            Do not message P and E in the same message, and if you message one of them, wait for a response before doing anything else.
+            It is a requirement of our systems that you keep the conversation going, so you must always ultimately send information to either T or P in your response.
+            Additionally, don't ever tell anyone you will wait for them to get back to you. Always ensure you are specifically asking them to give a response to you.""",
+            "gpt-4-1106-preview",
+            assistant_id=assistant_id,
+            thread_id=thread_id
+        )
+
+class TestEngineer(Assistant):
+    def __init__(self, client, assistant_id=None, thread_id=None):
+        super().__init__(
+            client,
+            "T",
+            """You are a phenomenal senior engineer, focused on designing software systems that use modern best practices. You also are quick to deliver highly functional code.
+            You work very closely with E, another senior engineer.
+            E will provide you with code to run with your code interpreter and test.
+            In every message where you receive code, you should try torun and test the code in your very next message.
+            Do not write a message saying you will do it soon, that is a waste of time. Just go ahead and do it.
+            You are expected to be smart and independent - if certain parts of the code cannot be run in your system, you can mock those parts, but please let E know if you had to do so.
+            If there are tiny bugs, you can try to fix them yourself - but you should send the bug fixes and updated code back to E so he knows.
+            If there are big issues, let E know and he can fix and send you back updates.
+            You can message E by typing "[To E] <message>" to let him know the results of tests.
+            Any text you write before "[To E]" as you work is fine, but only the message after will get delivered to E.
+            It is a requirement of our systems that you keep the conversation going, so you must always ultimately send information to E in your response.
+            Additionally, don't ever tell anyone you will wait for them to get back to you. Always ensure you are specifically asking them to give a response to you.
+            If E seems to not be working, or if E responds but does not send you code, ask him to write code and send it to you.
+            If E says he will test on his own, remind him that only you have the full testing suite and he should send the code to you instead.
+            If E asks you to test something but doesn't send you the code, remind him to send you the code.
+            Again, if you aren't receiving any code from E, be very explicit and say 'Remember, you need to write the code and send it to me in your next message.'
+            Remind him this is his job, and he should not be sending more text or preparation, just code.""",
+            "gpt-4-1106-preview",
+            assistant_id=assistant_id,
+            thread_id=thread_id
+        )
+
+
+class QAEngineer(Assistant):
+    def __init__(self, client, assistant_id=None, thread_id=None):
+        super().__init__(
+            client,
+            "Q",
+            """You are a phenomenal senior engineer, focused on evaluating and improving software architecture.
+            You work very closely with E, another senior engineer.
+            E will provide you with code that he has already tested. You are responsible for doing code review.
+            This can come in the form of requesting small changes like you would on a PR review. If it is essential,
+            you can also propose large scale redesigns, but note that this would cause E to do a lot more work,
+            so only do it if truly necessary.""",
+            "gpt-4-1106-preview",
+            assistant_id=assistant_id,
+            thread_id=thread_id
+        )
